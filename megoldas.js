6b593080a6f742a2534aed986a6f44fcf4182a75
amakkland <ogreg00@gmail.com>
2022-06-16 11:25:58 +0200

    TypeScript feladatok

---
+++ megoldas.js
@@ -0,0 +1,41 @@
+// 1.feladat
+function DiakInfo(nev, csoport, tipus) {
+    if (tipus == true) {
+        return nev + "[team" + csoport + "] Junior Frontend";
+    }
+    return nev + "[team" + csoport + "] Webfejlesztő";
+}
+document.write(DiakInfo("Minta Márton", 10, true));
+document.write(DiakInfo("<hr>Aki Valaki", 10, false));
+//2.feladat
+function szovegesErtekeles(jegy) {
+    if (jegy == 5) {
+        return ["Példás", "Példás"];
+    }
+    else if (jegy == 4) {
+        return ["Jó", "Jó"];
+    }
+    else if (jegy == 3) {
+        return ["Közepes", "Közepes"];
+    }
+    else if (jegy == 2) {
+        return ["Megfelelt", "Megfelelt"];
+    }
+    else {
+        return ["Értékelhetetlen", "Értékelhetetlen"];
+    }
+}
+var ertekeles = szovegesErtekeles(1);
+document.write("<hr>A tanuló szorgalom értékelése:" + ertekeles[0]);
+document.write("<hr>A tanuló magatartás értékelése:" + ertekeles[1]);
+//3.feladat
+function harommalOszthatoSzamok(tomb) {
+    var harommalOszthatokSzama = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        if (tomb[i] % 3 == 0) {
+            harommalOszthatokSzama++;
+        }
+    }
+    return harommalOszthatokSzama;
+}
+document.write("<hr> Harommal osztható elemk száma a tömbben:" + harommalOszthatoSzamok([10, 23, 12, 24, 31, 33, 42, 20]));
